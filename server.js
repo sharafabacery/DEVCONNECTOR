cff69628c82dab5745726a3527bba2c7150a7451
sharafabacery <52586357+sharafabacery@users.noreply.github.com>
2020-09-13 19:21:46 +0200

    Posts & CommentsPrepare

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const path=require('path')
 const express=require('express')
 const cors = require('cors')
 const connectDB=require('./config/db')
 const app=express()
 app.use(cors())
  
-const PORT=process.env.PORT ||5000
+
 connectDB()
 
 app.use(express.json({extended:false}))//get data delete body-parser packege
@@ -14,11 +15,14 @@ app.use('/api/profile',require("./routes/api/profile"))
 app.use('/api/posts',require("./routes/api/posts"))
 app.use('/api/auth',require("./routes/api/auth"))
 
+//serve static files
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
-
-
-
-
-
-
+const PORT=process.env.PORT ||5000
 app.listen(PORT,()=>{console.log("1")})
